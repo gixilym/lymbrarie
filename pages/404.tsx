ff74a38f3efaa47988e42885f0b8fa52fc01cfc7
gixi <gixipixel@gmail.com>
2024-06-13 16:50:33 -0300

    traducciones para 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,10 @@
 import type { Component } from "@/utils/types";
 import Link from "next/link";
+import { useTranslation } from "react-i18next";
 
 function NotFound(): Component {
+  const [t] = useTranslation("global");
+
   return (
     <section className="grid place-items-center text-white">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 mt-20">
@@ -10,14 +13,14 @@ function NotFound(): Component {
             404
           </p>
           <p className="mb-4 text-3xl tracking-tight font-semibold ">
-            La ruta especificada no existe
+            {t("page-not-found")}
           </p>
 
           <Link
             href="/"
             className="inline-flex bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-md px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
           >
-            <u className="text-xl">Inicio</u>
+            <u className="text-xl">{t("home")}</u>
           </Link>
         </div>
       </div>
