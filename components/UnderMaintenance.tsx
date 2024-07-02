9e8d924c42b97c6d79d7f1d73ea04bed883caf7d
gixi <gixipixe@gmail.com>
2024-07-02 03:12:59 -0300

    icono para el modo mantenimiento

--- components/UnderMaintenance.tsx
+++ components/UnderMaintenance.tsx
@@ -1,10 +1,11 @@
 import type { Component } from "@/utils/types";
 import { useTranslation } from "react-i18next";
-
+import { PaintRoller as PaintIcon } from "lucide-react";
 function UnderMaintenance(): Component {
   const [t] = useTranslation("global");
   return (
     <div className="flex flex-col justify-center items-center gap-y-12 w-full pt-20 h-full">
+      <PaintIcon size={90} />
       <h2 className="text-3xl md:text-5xl font-bold w-full text-center text-balance">
         {t("lymbrarie-is-under-maintenance")}
       </h2>
