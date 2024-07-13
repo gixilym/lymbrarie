61168a0d8782e74ea7bc3de441195ed2e82e3f89
gixi <gixipixe@gmail.com>
2024-07-13 02:25:15 -0300

    probando cuarta solución

--- utils/store.ts
+++ utils/store.ts
@@ -25,7 +25,7 @@ const popupsValue: RecoilState<any> = atom({
 
 const zeroBooksValue: RecoilState<boolean> = atom({
   key: "zeroBooksValue",
-  default: false,
+  default: true,
 });
 
 export { inputSearch, popupsValue, stateBookValue, zeroBooksValue };
