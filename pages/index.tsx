92c812ffc6902d7d5188097c142163ae7210641b
gixi <gixipixe@gmail.com>
2024-07-15 03:05:01 -0300

    intento 13

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,162 +1,131 @@
-import React from "react";
-import { useUser, withUser, withUserTokenSSR } from "next-firebase-auth";
+import AddYourFirstBook from "@/components/AddYourFirstBook";
+import FooterIndex from "@/components/FooterIndex";
+import HeaderIndex from "@/components/HeaderIndex";
+import ListSection from "@/components/ListSection";
+import LoadComponent from "@/components/LoadComponent";
+import Maintenance from "@/components/Maintenance";
+import PopUps from "@/components/PopUps";
+import { COLLECTION, EXAMPLES_BOOKS, MAINTENANCE } from "@/utils/consts";
+import useLocalStorage from "@/utils/hooks/useLocalStorage";
+import { zeroBooksValue } from "@/utils/store";
+import type { Book, Component, Doc } from "@/utils/types";
+import { animated, useSpring } from "@react-spring/web";
+import {
+  Query,
+  QuerySnapshot,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore/lite";
+import { AuthAction, useUser, withUser, withUserSSR } from "next-firebase-auth";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 
-const styles = {
-  content: {
-    padding: 32,
-  },
-  infoTextContainer: {
-    marginBottom: 32,
-  },
-};
+export default withUser({
+  whenAuthed: AuthAction.RENDER,
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+})(Index);
 
-const Demo = () => {
-  const user = useUser();
+function Index(): Component {
+  const user = useUser(),
+    [myBooks, setMyBooks] = useState<Book[]>([]),
+    isLogged: boolean = user != null,
+    UID: string = user.id as string,
+    profileImg: string = user?.photoURL as string,
+    profileName: string = user?.displayName as string,
+    [cacheBooks, setCacheBooks] = useLocalStorage("cacheBooks", null),
+    [, setAllTitles] = useLocalStorage("allTitles", []),
+    [booksIsEmpty, setBooksIsEmpty] = useState<boolean | null>(null),
+    [animations] = useLocalStorage("animations", true),
+    [loading, setLoading] = useState<boolean>(false),
+    [zeroBooks] = useRecoilState<boolean>(zeroBooksValue),
+    [styles, animate] = useSpring(() => ({
+      opacity: animations ? 0 : 1,
+      config: { duration: 1000 },
+    }));
+  console.log(UID);
 
-  console.log(JSON.stringify(user));
+  /* [, setSearch] = useRecoilState<string>(inputSearch),
+    auth: Auth = getAuth(),
+    router: NextRouter = useRouter(),
+    guest: boolean = JSON.parse((router.query.guest as string) ?? "false"),
+
+  useEffect(() => {
+    if (isLogged && guest) {  
+      setSearch("");
+      setAllTitles([]);
+      setCacheBooks(null);
+      auth.signOut().then(() => router.push("/login"));
+    }
+  }, [guest]);*/
+
+  useEffect(() => {
+    if (animations) animate.start({ opacity: 1 });
+  }, [animate]);
+
+  useEffect(() => {
+    if (zeroBooks) return;
+    if (isLogged && myBooks.length > 0) {
+      setCacheBooks(myBooks);
+      setAllTitles(myBooks.map((b: Book) => b.data.title));
+    }
+  }, [myBooks, user, zeroBooks]);
+
+  useEffect(() => {
+    if (isLogged) fetchBooks();
+    else setMyBooks(EXAMPLES_BOOKS);
+  }, [user]);
+
+  async function fetchBooks(): Promise<void> {
+    if (zeroBooks) return;
+    if (Array.isArray(cacheBooks)) setMyBooks(cacheBooks);
+    else {
+      setLoading(true);
+      const { books, isEmpty } = await getListBooks(UID);
+      setMyBooks(books);
+      setBooksIsEmpty(isEmpty);
+      setLoading(false);
+    }
+  }
+
   return (
-    <div>
-      <div style={styles.content}>
-        <div style={styles.infoTextContainer}>
-          <h3>Home</h3>
-          <p>
-            This page does not require authentication, so it wont redirect to
-            the login page if you are not signed in.
-          </p>
-          <p>
-            If you remove `getServerSideProps` from this page, it will be static
-            and load the authed user only on the client side.
-          </p>
-        </div>
-      </div>
-    </div>
+    <animated.div
+      style={styles}
+      className="flex flex-col justify-start items-center w-full sm:max-w-[950px] h-full gap-y-6"
+    >
+      <Head>
+        <title>Lymbrarie</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="donde cada libro encuentra su lugar"
+        />
+      </Head>
+
+      {!MAINTENANCE ? (
+        <>
+          <HeaderIndex isLogged={isLogged} />
+          <PopUps profileImg={profileImg} profileName={profileName} UID={UID} />
+          {loading ? (
+            <LoadComponent />
+          ) : (
+            <>
+              {booksIsEmpty || zeroBooks ? (
+                <AddYourFirstBook />
+              ) : (
+                <ListSection myBooks={myBooks} isLogged={isLogged} />
+              )}
+            </>
+          )}
+          <FooterIndex isLogged={isLogged} />
+        </>
+      ) : (
+        <Maintenance />
+      )}
+    </animated.div>
   );
-};
-
-export default withUser()(Demo);
-// import AddYourFirstBook from "@/components/AddYourFirstBook";
-// import FooterIndex from "@/components/FooterIndex";
-// import HeaderIndex from "@/components/HeaderIndex";
-// import ListSection from "@/components/ListSection";
-// import LoadComponent from "@/components/LoadComponent";
-// import Maintenance from "@/components/Maintenance";
-// import PopUps from "@/components/PopUps";
-// import { COLLECTION, EXAMPLES_BOOKS, MAINTENANCE } from "@/utils/consts";
-// import useLocalStorage from "@/utils/hooks/useLocalStorage";
-// import { zeroBooksValue } from "@/utils/store";
-// import type { Book, Component, Doc } from "@/utils/types";
-// import { animated, useSpring } from "@react-spring/web";
-// import {
-//   Query,
-//   QuerySnapshot,
-//   getDocs,
-//   query,
-//   where,
-// } from "firebase/firestore/lite";
-// import { AuthAction, withUser, withUserSSR } from "next-firebase-auth";
-// import Head from "next/head";
-// import { useEffect, useState } from "react";
-// import { useRecoilState } from "recoil";
-
-// export default withUser<Props>()(Index);
-
-// function Index({ user }: Props): Component {
-//   const [myBooks, setMyBooks] = useState<Book[]>([]),
-//     isLogged: boolean = user != null,
-//     UID: string = user?.UID as string,
-//     profileImg: string = user?.img as string,
-//     profileName: string = user?.name as string,
-//     [cacheBooks, setCacheBooks] = useLocalStorage("cacheBooks", null),
-//     [, setAllTitles] = useLocalStorage("allTitles", []),
-//     [booksIsEmpty, setBooksIsEmpty] = useState<boolean | null>(null),
-//     [animations] = useLocalStorage("animations", true),
-//     [loading, setLoading] = useState<boolean>(false),
-//     [zeroBooks] = useRecoilState<boolean>(zeroBooksValue),
-//     [styles, animate] = useSpring(() => ({
-//       opacity: animations ? 0 : 1,
-//       config: { duration: 1000 },
-//     }));
-
-//   /* [, setSearch] = useRecoilState<string>(inputSearch),
-//     auth: Auth = getAuth(),
-//     router: NextRouter = useRouter(),
-//     guest: boolean = JSON.parse((router.query.guest as string) ?? "false"),
-
-//   useEffect(() => {
-//     if (isLogged && guest) {
-//       setSearch("");
-//       setAllTitles([]);
-//       setCacheBooks(null);
-//       auth.signOut().then(() => router.push("/login"));
-//     }
-//   }, [guest]);*/
-
-//   useEffect(() => {
-//     if (animations) animate.start({ opacity: 1 });
-//   }, [animate]);
-
-//   useEffect(() => {
-//     if (zeroBooks) return;
-//     if (isLogged && myBooks.length > 0) {
-//       setCacheBooks(myBooks);
-//       setAllTitles(myBooks.map((b: Book) => b.data.title));
-//     }
-//   }, [myBooks, user, zeroBooks]);
-
-//   useEffect(() => {
-//     if (isLogged) fetchBooks();
-//     else setMyBooks(EXAMPLES_BOOKS);
-//   }, [user]);
-
-//   async function fetchBooks(): Promise<void> {
-//     if (zeroBooks) return;
-//     if (Array.isArray(cacheBooks)) setMyBooks(cacheBooks);
-//     else {
-//       setLoading(true);
-//       const { books, isEmpty } = await getListBooks(UID);
-//       setMyBooks(books);
-//       setBooksIsEmpty(isEmpty);
-//       setLoading(false);
-//     }
-//   }
-
-//   return (
-//     <animated.div
-//       style={styles}
-//       className="flex flex-col justify-start items-center w-full sm:max-w-[950px] h-full gap-y-6"
-//     >
-//       <Head>
-//         <title>Lymbrarie</title>
-//         <meta name="viewport" content="width=device-width, initial-scale=1" />
-//         <meta
-//           name="description"
-//           content="donde cada libro encuentra su lugar"
-//         />
-//       </Head>
-
-//       {!MAINTENANCE ? (
-//         <>
-//           <HeaderIndex isLogged={isLogged} />
-//           <PopUps profileImg={profileImg} profileName={profileName} UID={UID} />
-//           {loading ? (
-//             <LoadComponent />
-//           ) : (
-//             <>
-//               {booksIsEmpty || zeroBooks ? (
-//                 <AddYourFirstBook />
-//               ) : (
-//                 <ListSection myBooks={myBooks} isLogged={isLogged} />
-//               )}
-//             </>
-//           )}
-//           <FooterIndex isLogged={isLogged} />
-//         </>
-//       ) : (
-//         <Maintenance />
-//       )}
-//     </animated.div>
-//   );
-// }
+}
 
 // export const getServerSideProps = withUserSSR({
 //   whenAuthed: AuthAction.RENDER,
@@ -173,37 +142,37 @@ export default withUser()(Demo);
 //   return { props: { user } };
 // });
 
-// async function getListBooks(UID: string): Promise<List> {
-//   const books: Book[] = [];
-//   let isEmpty: boolean = false;
+async function getListBooks(UID: string): Promise<List> {
+  const books: Book[] = [];
+  let isEmpty: boolean = false;
 
-//   if (UID) {
-//     try {
-//       const q: Query = query(COLLECTION, where("owner", "==", UID));
-//       const res: QuerySnapshot = await getDocs(q);
-//       isEmpty = res.empty;
-//       res.forEach((doc: Doc) => books.push({ id: doc.id, data: doc.data() }));
-//     } catch (err: any) {
-//       if (!MAINTENANCE) {
-//         console.error(err.message);
-//         // const type: string =
-//         //   err.message == "Quota exceeded." ? "limit" : "unknown";
-//         // location.href = `/error?err=${type}`;
-//       }
-//     }
-//   }
-//   return { books, isEmpty };
-// }
+  if (UID) {
+    try {
+      const q: Query = query(COLLECTION, where("owner", "==", UID));
+      const res: QuerySnapshot = await getDocs(q);
+      isEmpty = res.empty;
+      res.forEach((doc: Doc) => books.push({ id: doc.id, data: doc.data() }));
+    } catch (err: any) {
+      if (!MAINTENANCE) {
+        console.error(err.message);
+        // const type: string =
+        //   err.message == "Quota exceeded." ? "limit" : "unknown";
+        // location.href = `/error?err=${type}`;
+      }
+    }
+  }
+  return { books, isEmpty };
+}
 
-// interface Props {
-//   user: User | null;
-// }
+interface Props {
+  user: User | null;
+}
 
-// interface User {
-//   UID: string | null;
-//   email: string | null;
-//   name: string | null;
-//   img: string | null;
-// }
+interface User {
+  UID: string | null;
+  email: string | null;
+  name: string | null;
+  img: string | null;
+}
 
-// type List = { books: Book[]; isEmpty: boolean };
+type List = { books: Book[]; isEmpty: boolean };
