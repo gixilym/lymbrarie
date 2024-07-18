8d8453608dc10685a5102c2c134a78afb001e5c9
gixi <gixipixe@gmail.com>
2024-07-18 01:35:16 -0300

    animación para AppIcon

--- components/AppIcon.tsx
+++ components/AppIcon.tsx
@@ -1,18 +1,31 @@
 import icon from "@/public/favicon.ico";
+import useLocalStorage from "@/utils/hooks/useLocalStorage";
 import type { Component } from "@/utils/types";
+import { animated, useSpring } from "@react-spring/web";
 import Image from "next/image";
-import Link from "next/link";
+import { useEffect } from "react";
 
 function AppIcon(): Component {
+  const [animations] = useLocalStorage("animations", true);
+  const [styles, animate] = useSpring(() => ({
+    opacity: animations ? 0 : 1,
+    config: { duration: 500 },
+  }));
+
+  useEffect(() => {
+    if (animations) animate.start({ opacity: 1 });
+  }, [animate]);
+
   return (
-    <Link
+    <animated.a
+      style={styles}
       href="/"
       title="Lymbrarie"
       className="hidden sm:flex px-3.5 py-2 rounded-full cursor-default justify-between items-center bg-purple-700/10 fixed top-5 left-5 gap-x-3"
     >
       <Image src={icon} width={35} height={35} alt="logo" />
       <p className="text-xl text-purple-200/90">Lymbrarie</p>
-    </Link>
+    </animated.a>
   );
 }
 
