3a3e4d469296a0b67c07d305ab239bb7dab7b647
gixi <gixi.tsx@gmail.com>
2024-10-11 00:26:11 -0300

    probando web api share

--- pages/book/[bookId].tsx
+++ pages/book/[bookId].tsx
@@ -31,6 +31,7 @@ import {
   Library as LibraryIcon,
   Notebook as NotesIcon,
   Bookmark as RemoveFavIcon,
+  Share2,
   Tag as StateIcon,
   User as UserIcon,
 } from "lucide-react";
@@ -43,6 +44,7 @@ import toast from "react-hot-toast";
 import { useTranslation } from "react-i18next";
 import { useRecoilState } from "recoil";
 import { twMerge } from "tailwind-merge";
+import html2canvas from "html2canvas-pro";
 
 export default withUser({
   whenAuthed: AuthAction.RENDER,
@@ -156,14 +158,41 @@ function BookId(): Component {
     }
   }
 
+  // function handleShare() {
+  //   const container = document.getElementById("screenshot");
+  //   if (container) {
+  //     html2canvas(container, { backgroundColor: "rgb(2,6,23)" }).then(
+  //       canvas => {
+  //         const image = canvas.toDataURL("image/png");
+  //         const link = document.createElement("a");
+  //         link.href = image;
+  //         link.download = bookTitle;
+  //         link.click();
+  //       }
+  //     );
+  //   }
+  // }
+
+  function handleShare() {
+    const info = {
+      title,
+      text: "Lymbrarie - donde cada libro encuentra su lugar",
+      url: "https://lymbrarie.com",
+    };
+    if (navigator.share) {
+      navigator
+        .share(info)
+        .then(() => console.log("Compartido"))
+        .catch(err => console.error(err));
+    } else alert("navegador no compatible");
+  }
+
   return (
     <animated.section
       style={stylesSection}
-      className="flex flex-col justify-center items-center w-full gap-y-6 sm:pb-10 sm:pt-20 h-full sm:pl-20"
-    >
+      className="flex flex-col justify-center items-center w-full gap-y-6 sm:pb-10 sm:pt-20 h-full sm:pl-20">
       <Head>
         <title translate="no">{book?.data?.title || "Lymbrarie"}</title>
-        <meta name="google-adsense-account" content="ca-pub-6333479205921641" />
       </Head>
 
       {popup.offline && <OfflinePopUp />}
@@ -178,7 +207,9 @@ function BookId(): Component {
 
       <BackBtn hidden />
       <Breadcrumbs title={book?.data?.title ?? ""} />
-      <article className="w-full sm:w-[700px] h-[300px] flex flex-col sm:flex-row gap-y-12 justify-start items-center sm:items-start backdrop-blur-[2.5px] relative mt-20 xl:mt-0 sm:mt-12">
+      <article
+        id="screenshot"
+        className="w-full sm:w-[700px] h-[300px] flex flex-col sm:flex-row gap-y-12 justify-start items-center sm:items-start backdrop-blur-[2.5px] relative mt-20 xl:mt-0 sm:mt-12">
         <Cover
           priority
           style={stylesImg}
@@ -226,12 +257,10 @@ function BookId(): Component {
           </div>
           <animated.div
             style={stylesIcons}
-            className="flex items-center justify-center gap-x-2"
-          >
+            className="flex items-center justify-center gap-x-2">
             <button
               onClick={() => openPopUp("notes")}
-              className="btn btn-square bg-slate-700/30 sm:bg-slate-700/25 hover:bg-slate-700/50 border-2 border-slate-700/40 mt-4 sm:mt-0 mb-1"
-            >
+              className="btn btn-square bg-slate-700/30 sm:bg-slate-700/25 hover:bg-slate-700/50 border-2 border-slate-700/40 mt-4 sm:mt-0 mb-1">
               <NotesIcon className="w-6 h-6 sm:w-7 sm:h-8" />
             </button>
             <div className="dropdown dropdown-top dropdown-right opacity-100 flex sm:block items-end justify-center">
@@ -241,13 +270,11 @@ function BookId(): Component {
                 className={twMerge(
                   loadingFav ? "hidden" : "block",
                   "mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-[240px]"
-                )}
-              >
+                )}>
                 <li
                   onClick={() =>
                     navigator.onLine ? toggleFav() : openPopUp("offline")
-                  }
-                >
+                  }>
                   <div className="flex flex-row items-center justify-start gap-x-3">
                     {checkFav ? (
                       <FavoriteIcon size={18} />
@@ -264,8 +291,7 @@ function BookId(): Component {
                     navigator.onLine
                       ? openPopUp("edit_book")
                       : openPopUp("offline")
-                  }
-                >
+                  }>
                   <div className="flex flex-row items-center justify-start gap-x-3">
                     <EditIcon size={18} />
                     <p>{t("edit-book")}</p>
@@ -277,8 +303,7 @@ function BookId(): Component {
                     navigator.onLine
                       ? openPopUp("delete_book")
                       : openPopUp("offline")
-                  }
-                >
+                  }>
                   <div className="flex flex-row items-center justify-start gap-x-3">
                     <DeleteIcon size={18} />
                     <p>{t("delete-book")}</p>
@@ -286,6 +311,11 @@ function BookId(): Component {
                 </li>
               </ul>
             </div>
+            <button
+              onClick={handleShare}
+              className="btn btn-square bg-slate-700/30 sm:bg-slate-700/25 hover:bg-slate-700/50 border-2 border-slate-700/40 mb-1 mt-4 sm:mt-0 ">
+              <Share2 className="w-5 h-5 sm:w-[26px] sm:h-[26px]" />
+            </button>
           </animated.div>
         </div>
       </article>
