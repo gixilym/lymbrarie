1bd25202586c1c71598f6413d8930ca041a18c70
Gixi <gixi.tsx@gmail.com>
2025-02-17 16:57:44 -0300

    menos wdt

--- components/AppIcon.tsx
+++ components/AppIcon.tsx
@@ -21,13 +21,13 @@ function AppIcon(): Component {
         style={styles}
         className="hidden lg:flex px-3.5 py-2 rounded-full border-purple-700/10 border-2 cursor-default justify-between items-center bg-purple-700/10 fixed top-9.5 left-5 gap-x-3"
       >
-        <Image src={Favicon.src} width={35} height={35} alt="logo" />
+        <Image src={Favicon.src} width={33} height={33} alt="logo" />
         <p className="text-xl text-purple-200/90" translate="no">
           Lymbrarie
         </p>
       </MyLink>
       <div className="flex lg:hidden w-[93%] max-w-3xl justify-center gap-x-3 items-center py-2 rounded-2xl mb-4  border-purple-700/10 border-2 bg-purple-700/10">
-        <Image src={Favicon.src} width={35} height={35} alt="logo" />
+        <Image src={Favicon.src} width={33} height={33} alt="logo" />
         <p className="text-xl text-purple-200/90" translate="no">
           Lymbrarie
         </p>
